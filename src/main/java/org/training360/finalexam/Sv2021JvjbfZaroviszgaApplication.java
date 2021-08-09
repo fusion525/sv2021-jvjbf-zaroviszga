82fbb5a20ff3ca0a3ac900ddf6f53123c3c2babb
fusion525 <fusion525@users.noreply.github.com>
2021-08-09 17:34:24 +0200

    Initial commit

---
+++ src/main/java/org/training360/finalexam/Sv2021JvjbfZaroviszgaApplication.java
@@ -0,0 +1,13 @@
+package org.training360.finalexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sv2021JvjbfZaroviszgaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sv2021JvjbfZaroviszgaApplication.class, args);
+    }
+
+}
